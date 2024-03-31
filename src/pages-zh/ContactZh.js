b34b47c6e61e5442381ecb58ccfd15c97f141a29
林劭寰 <linshaohuan@linshaohuandeMacBook-Air.local>
2024-03-31 13:12:59 +0800

    add all chinese version website"

---
+++ src/pages-zh/ContactZh.js
@@ -0,0 +1,28 @@
+import HeaderZH from '../components/Header-zh';
+import FooterZH from '../components/Footer-zh';
+
+function ContactZH() {
+    return (
+        <div className="landing-backgrounds">
+            <HeaderZH />
+            <div className='white-mocks'>
+                <div className="about-page">
+                    <div className="About-content">
+                        <br/>
+                        <br/>
+                        <div className='FAQ-head'>
+                            <h3>聯絡我們</h3>
+                        </div>
+                        <div className='FAQ-Block'>
+
+                        </div>
+                    </div>
+                </div>
+                <FooterZH />
+            </div>
+        </div>
+    )
+}
+
+
+export default ContactZH
---
+++ src/pages-zh/Services/CompanyZh.js
@@ -0,0 +1,46 @@
+import HeaderZH from '../components/Header-zh';
+import FooterZH from '../components/Footer-zh';
+
+
+function CompanyZH() {
+    return (
+        <div className="landing-backgrounds">
+            <HeaderZH />
+            <div className='white-mocks'>
+                <div className="about-page">
+                    <div className="About-content">
+                        <br/>
+                        <br/>
+                        <div className='FAQ-head'>
+                            <h3>公司登記與註冊服務</h3>
+                        </div>
+                        <div className='FAQ-Block'>
+                            <div>
+                            在香港、開曼群島、英國維爾京群島（BVI）以及塞席爾的公司註冊與登記服務。
+                            <br/>
+                            我們的服務包含
+                            <br/>
+                            公司的成立和註冊
+                            董監事和執行長、經理或一般合夥人的任命
+                            向成員發行股份，向有限合夥人或成員發行利息給商業公司
+                            面對的挑戰：
+                            <br/>
+                            著手進行國際業務設置涉及瀏覽復雜的本地法律和公司治理指南，往往使企業難以應對細節。
+                            簡化方法：
+                            <br/>
+                            我們的解決方案提出了一個簡化過程，快速進行離岸公司成立，引入一個用戶友好的客戶門戶，以簡化整體體驗並通過高效和簡化的過程加速成立。
+                            為成功鋪平道路：
+                            讓我們成為您邁向國際業務成功的夥伴。
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <FooterZH />
+            </div>
+        </div>
+    )
+}
+
+
+export default CompanyZH
